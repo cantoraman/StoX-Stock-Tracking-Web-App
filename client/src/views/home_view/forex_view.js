25f7b854e3dd04463fad1b9010ac687763ce2b2c
leeburgess67 <leeburgessmail@gmail.com>
2018-08-18 18:29:01 +0100

    forex table displaying correctly

--- client/src/views/home_view/forex_view.js
+++ client/src/views/home_view/forex_view.js
@@ -6,27 +6,55 @@ const ForexView = function (container) {
 
 ForexView.prototype.bindEvents = function () {
   PubSub.subscribe('Forex:publish-listOfPrices', (evt) => {
-    this.render(evt.detail);
+    this.createTable(evt.detail);
   });
 };
 
-ForexView.prototype.render = function (data) {
-  this.container.innerHTML = '';
-console.log(data);
-  data.forEach((item) => {
-    this.container.appendChild(this.createItem(item));
-  })
+// ForexView.prototype.render = function (data) {
+//   this.container.innerHTML = '';
+//   data.forEach((item) => {
+//     this.container.appendChild(this.createItem(item));
+//   })
 
-};
+// };
 
-ForexView.prototype.createItem = function (item) {
-  const itemNode = document.createElement('ul');
-  const pairNode = document.createElement('p');
-  itemNode.classList.add('forex-item');
-  itemNode.textContent = item.price;
-  pairNode.textContent = item.symbol;
-  itemNode.appendChild(pairNode);
-  return itemNode;
+ForexView.prototype.createTable = function (data) {
+
+  // const itemNode = document.createElement('ul');
+  // const pairNode = document.createElement('p');
+  // itemNode.classList.add('forex-item');
+  // itemNode.textContent = item.price;
+  // pairNode.textContent = item.symbol;
+  // itemNode.appendChild(pairNode);
+  // return itemNode;
+
+  const forexTable = document.createElement('table');
+  forexTable.classList.add('forex-table');
+  this.container.appendChild(forexTable);
+  const tableHeader = forexTable.insertRow(0);
+  const nameHeader = tableHeader.insertCell(0);
+  const priceHeader = tableHeader.insertCell(1);
+
+  const tradingPrice = [];
+  const forexName = [];
+
+  i = 0;
+  data.forEach(function(day) {
+    forexName.push(data[i].symbol);
+    tradingPrice.push(data[i].price);
+    const row = forexTable.insertRow(1);
+    tableHeader.classList.add('forex-header');
+
+    const nameCell = row.insertCell(0);
+    const priceCell = row.insertCell(1);
+    nameCell.innerHTML = data[i].symbol;
+    priceCell.innerHTML = data[i].price.toFixed(2);
+    i++
+  });
+
+
+  nameHeader.innerHTML = "Forex";
+  priceHeader.innerHTML = "Price";
 };
 
 module.exports = ForexView;
