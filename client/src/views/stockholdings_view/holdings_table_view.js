527013f6e74dac0aac6e4a56d0bc5b315d55b0ad
Magdalena <m.mandat@gmail.com>
2018-08-20 12:26:25 +0100

    popup button added

--- client/src/views/stockholdings_view/holdings_table_view.js
+++ client/src/views/stockholdings_view/holdings_table_view.js
@@ -21,6 +21,8 @@ this.renderHoldings(userData[0].holdings, this.container);
 
 HoldingsTableView.prototype.renderHoldings = function (userData, pageBody) {
 
+        generatePopupForm();
+
   const holdingsTable = document.createElement('table');
   holdingsTable.classList.add('holdings-table');
   this.container.appendChild(holdingsTable);
@@ -31,12 +33,15 @@ HoldingsTableView.prototype.renderHoldings = function (userData, pageBody) {
   const currentValueHeader = tableHeader.insertCell(2);
   const sharesHeldHeader = tableHeader.insertCell(3);
   const profitLossHeader = tableHeader.insertCell(4);
+  const addHeader = tableHeader.insertCell(5);
+  const removeHeader = tableHeader.insertCell(6);
   // stock, invested value, shares held, profit/loss
   const stockNames = [];
   const stockValues = [];
   const stockCurrentValue = [];
   const sharesHeld = [];
   const profitLoss = [];
+
 
   i = 0;
 
@@ -54,25 +59,77 @@ HoldingsTableView.prototype.renderHoldings = function (userData, pageBody) {
     const stockCurrentValueCell = row.insertCell(2);
     const sharesHeldCell = row.insertCell(3);
     const profitLossCell = row.insertCell(4);
+    const addCell = row.insertCell(5);
+    const removeCell = row.insertCell(6);
     stockNamesCell.innerHTML = stock.stock;
     stockValuesCell.innerHTML = stock.investedValue;
     stockCurrentValueCell.innerHTML = 100;
     sharesHeldCell.innerHTML = stock.noOfSharesHeld;
     profitLossCell.innerHTML = stock.profitLoss;
-
+    addCell.innerHTML = "Add";
+    addCell.classList.add("indicator");
+    removeCell.innerHTML = "Remove";
+    removeCell.classList.add("indicator");
+    addCell.addEventListener('click', (event) => {
+      console.log("add button pressed");
+      popThePopup();
+    });
+    removeCell.addEventListener('click', (event) => {
+      console.log("remove button pressed");
+    });
     i++
   });
 
+
+
+
+function generatePopupForm() {
+  const container = document.createElement('div');
+  container.classList.add("popup");
+  const body = document.querySelector("#pageBody");
+  body.appendChild(container);
+
+  const span = document.createElement('span');
+  span.id="myPopup";
+  span.classList.add("popuptext");
+  span.textContent = "HELLO WORLD";
+  container.appendChild(span);
+
+  const closeButton = document.createElement('button');
+  closeButton.id = "close-button";
+  span.appendChild(closeButton);
+  closeButton.addEventListener('click', (event) => {
+  togglePopup();
+});
+  //
+  // const form = document.createElement('form');
+  // const input = document.createElement('input');
+  //
+  //
+  // //
+  // form.classList.add = "myPopup" ;
+  // var popup = document.querySelector("myPopup");
+    //popup.classList.toggle("show");
+};
+
+function togglePopup(){
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+
+}
 
   nameHeader.innerHTML = "Stock";
   valueHeader.innerHTML = "Invested Value";
   currentValueHeader.innerHTML = "Current Price";
   sharesHeldHeader.innerHTML = "Volume";
   profitLossHeader.innerHTML = "Profit/Loss";
-
+  addHeader.innerHTML = "Bought";
+  removeHeader.innerHTML = "Sold";
 
 
 
 };
+
+
 
 module.exports = HoldingsTableView;
