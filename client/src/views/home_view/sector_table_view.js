8f520b510c7ddeaf2df95bb599263a7da4043ceb
leeburgess67 <leeburgessmail@gmail.com>
2018-08-22 11:08:08 +0100

    holdings table showing

--- client/src/views/home_view/sector_table_view.js
+++ client/src/views/home_view/sector_table_view.js
@@ -5,24 +5,37 @@ const SectorTableView = function (container) {
 }
 
 SectorTableView.prototype.bindEvents = function () {
-  PubSub.subscribe('SectorTable:publish-sector', (evt) => {
-    console.log(evt);
+  PubSub.subscribe('SectorTable:publish-sector-data', (evt) => {
     this.createSectorTable(evt.detail);
   });
 }
 
-  SectorTableView.prototype.createSectorTable = function (sectors) {
-    const sectorTable = document.createElement('table');
-    sectorTable.classList.add('sector-table');
-    this.container.appendChild(sectorTable);
+SectorTableView.prototype.createSectorTable = function (data) {
+  const sectorTable = document.createElement('table');
+  sectorTable.classList.add('sector-table');
+  this.container.appendChild(sectorTable);
 
-    const tableHeader = sectorTable.insertRow(0);
-    tableHeader.innerHTML = "Sector";
+  const tableHeader = sectorTable.insertRow(0);
 
-    const performanceHeader = sectorTable.insertRow(1);
-    tableHeader.innerHTML = "Yesterday's Performance";
+  const nameHeader = tableHeader.insertCell(0);
+  nameHeader.textContent = "Sector performance"
+  const performanceHeader = tableHeader.insertCell(1);
 
+  const sectorName = [];
+  const sectorPerformance =[]
 
-
+  data.reverse();
+  data.forEach(function(day, i) {
+    const row = sectorTable.insertRow(1);
+    sectorName.push(data[i].name);
+    sectorPerformance.push(data[i].performance);
+  const nameCell = row.insertCell(0);
+  nameCell.classList.add("indicator");
+  nameCell.textContent = data[i].name;
+  const priceCell = row.insertCell(1);
+  priceCell.innerHTML = data[i].performance;
+    });
+  nameHeader.innerHTML = "Sector";
+  performanceHeader.innerHTML = "Performance";
 };
 module.exports = SectorTableView;
