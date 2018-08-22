79272ab576f4cc1e7660c68b1399d383eb73c6ef
leeburgess67 <leeburgessmail@gmail.com>
2018-08-22 16:08:42 +0100

    watchlist prices displaying

--- client/src/views/watchlist_view/watchlist_table_view.js
+++ client/src/views/watchlist_view/watchlist_table_view.js
@@ -6,38 +6,58 @@ const WatchlistTableView = function (container) {
 }
 
 WatchlistTableView.prototype.bindEvents = function () {
-  PubSub.subscribe('WatchlistTableView:data-loaded', (evt) => {
-    this.renderWatchlist(evt);
+
+
+};
+
+WatchlistTableView.prototype.initializeTable = function () {
+
+  PubSub.subscribe('AppData:watchlist-prices-array-loaded', (prices) => {
+    const watchListPrices = prices.detail[0];
+    const watchListNames = prices.detail[1];
+    this.renderWatchlist(watchListPrices, watchListNames)
   });
 };
 
-WatchlistTableView.prototype.initializeTable = function (userData) {
 
-  this.renderWatchlist(userData[0].watchList, this.container);
-};
 
-WatchlistTableView.prototype.renderWatchlist = function (userData, pageBody) {
+WatchlistTableView.prototype.renderWatchlist = function (prices, names) {
 
+console.log(names);
   const watchlistTable = document.createElement('table');
   watchlistTable.classList.add('watchlist-table');
   this.container.appendChild(watchlistTable);
   const tableHeader = watchlistTable.insertRow(0);
   const nameHeader = tableHeader.insertCell(0);
+  nameHeader.textContent = "Watching";
+  const priceHeader = tableHeader.insertCell(1);
+  priceHeader.textContent = "Latest Price";
+
 
   const stockNames = [];
-  console.log(userData);
+  const stockPrices = [];
 
-userData.forEach(function(stock) {
-  stockNames.push(stock);
-  const row = watchlistTable.insertRow(1);
-  row.addEventListener('click', (event) => {
-    PubSub.publish('Graph:request-graphdata', stock);
+  names.forEach(function(stock, index) {
+    stockNames.push(stock);
+    const row = watchlistTable.insertRow(1);
+    row.addEventListener('click', (event) => {
+      PubSub.publish('Graph:request-graphdata', stock);
+    });
+    tableHeader.classList.add('watchlist-header');
+    const stockNamesCell = row.insertCell(0);
+    stockNamesCell.textContent = stock;
+    stockNamesCell.classList.add("indicator");
+
+    const price = prices[index]
+    stockPrices.push(price);
+    const priceCell = row.insertCell(1);
+    priceCell.textContent = price;
+
+
+
+
   });
-  tableHeader.classList.add('watchlist-header');
-  const stockNamesCell = row.insertCell(0);
-  stockNamesCell.textContent = stock;
-  stockNamesCell.classList.add("indicator");
-});
+
 
 
 };
